a51905659797dc207d0591c7637e1eb7fd346001
sdy329 <sdy329@gmail.com>
2024-10-27 00:59:30 -0400

    Fixed height for projects

--- src/app/AngularToDoList/page.jsx
+++ src/app/AngularToDoList/page.jsx
@@ -9,7 +9,7 @@ export default async function ResumePage() {
         <div className="bg-slate-900 text-slate-500 font-medium font-sans selection:text-sky-400">
             <div className="mx-auto min-h-screen font-sans">
                 <ProjectNavBar link="https://github.com/sdy329/angular-todo-list" />
-                <iframe src="https://angular-todo-list-psi.vercel.app" className="w-full h-screen" />
+                <iframe src="https://angular-todo-list-psi.vercel.app" className="w-full max-h-[90vh] md:h-[90vh] h-[75vh]" />
             </div>
         </div>
     );
