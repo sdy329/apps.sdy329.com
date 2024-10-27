46ca8d8fdf0f9ca99000790b1e7acc794f31c7ea
sdy329 <sdy329@gmail.com>
2024-10-26 23:29:40 -0400

    Editing metadata for pages

---
+++ src/app/AngularToDoList/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
+
+export const metadata = {
+  title: "AngularToDoList - Spencer Yates",
+  charset: "utf-8",
+  openGraph: {
+    locale: "en_US",
+    site_name: "AngularToDoList - Spencer Yates",
+    type: "website",
+    title: "AngularToDoList - Spencer Yates",
+    description: "The AngularToDoList is a simple to do list app built with Angular. The app was made to learn the basics of Angular.",
+    url: "https://apps.sdy329.com"
+  },
+};
---
+++ src/app/SpoilerGuard/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
+
+export const metadata = {
+  title: "SpoilerGuard - Spencer Yates",
+  charset: "utf-8",
+  openGraph: {
+    locale: "en_US",
+    site_name: "SpoilerGuard - Spencer Yates",
+    type: "website",
+    title: "SpoilerGuard - Spencer Yates",
+    description: "SpoilerGuard is A cross-platform wiki application that allows users to browse articles of their favorite media without fear of spoilers.",
+    url: "https://apps.sdy329.com"
+  },
+};
