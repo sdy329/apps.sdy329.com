a1efbb8571a46492bd34809b958238b2906961c8
sdy329 <sdy329@gmail.com>
2024-10-26 23:34:58 -0400

    New fix

--- src/app/AngularToDoList/layout.tsx
+++ src/app/AngularToDoList/layout.tsx
@@ -1,4 +1,8 @@
-import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const viewport = {
   width: 'device-width',
@@ -17,3 +21,15 @@ export const metadata = {
     url: "https://apps.sdy329.com"
   },
 };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
