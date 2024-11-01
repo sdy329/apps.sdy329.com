520bac8abe793be8c9631a5f22a338679222d786
sdy329 <sdy329@gmail.com>
2024-11-01 03:00:41 -0400

    Version 1.0.0 release

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,68 +1,52 @@
 import Link from "next/link";
 
 export default function ProjectCards() {
+    const projects = [
+        {
+            "title": "SpoilerGuard",
+            "description": "A cross-platform wiki application that allows users to browse articles of their favorite media without fear of spoilers.",
+            "cover": "/covers/SpoilerGuard.png",
+            "link": "/SpoilerGuard",
+        },
+        {
+            "title": "Angular ToDo List",
+            "description": "A simple to do list app built with Angular. The app was made to learn the basics of Angular.",
+            "cover": "/covers/AngularToDoList.png",
+            "link": "/Angular-To-Do-List",
+        },
+        {
+            "title": "Cross Platform Calculator",
+            "description": "A calculator app with many features and themes. The app was made as a part of a team using Dart and Flutter.",
+            "cover": "/covers/CPCalculator.png",
+            "link": "/Cross-Platform-Calculator",
+        },
+        {
+            "title": "EcoTrack",
+            "description": "EcoTrack is a web forum for discussing environmental issues and tracking progress on environmental initiatives.",
+            "cover": "/covers/EcoTrack.png",
+            "link": "/EcoTrack",
+        }
+    ];
+
     return (
-        <div className="flex flex-wrap justify-center">
-            <div className="m-4">
-                <Link href="/SpoilerGuard">
-                    <div className="z-10 w-full opacity-100 hover:opacity-85 transition-all motion-reduce:transition-none">
-                        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-700 min-h-60">
-                            <img className="w-full" src="/covers/SpoilerGuard.png" alt="SpoilerGuard" />
-                            <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2 text-slate-200">SpoilerGuard</div>
-                                <p className="text-gray-400 text-base">
-                                    A cross-platform wiki application that allows users to browse articles of their favorite media without fear of spoilers.
-                                </p>
+        <div className="flex flex-wrap justify-center" >
+            {projects.map((project) => (
+                <div className="m-4" key={project.title}>
+                    <Link href={project.link}>
+                        <div className="z-10 w-full opacity-100 hover:opacity-85 transition-all motion-reduce:transition-none">
+                            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-700 min-h-60">
+                                <img className="w-full" src={project.cover} alt="SpoilerGuard" />
+                                <div className="px-6 py-4">
+                                    <div className="font-bold text-xl mb-2 text-slate-200">{project.title}</div>
+                                    <p className="text-gray-400 text-base">
+                                        {project.description}
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="m-4">
-                <Link href="/Angular-To-Do-List">
-                    <div className="z-10 w-full opacity-100 hover:opacity-85 transition-all motion-reduce:transition-none">
-                        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-700 min-h-60">
-                            <img className="w-full" src="/covers/AngularToDoList.png" alt="Angular To Do List" />
-                            <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2 text-slate-200">Angular ToDo List</div>
-                                <p className="text-gray-400 text-base">
-                                    A simple to do list app built with Angular. The app was made to learn the basics of Angular.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="m-4">
-                <Link href="/Cross-Platform-Calculator">
-                    <div className="z-10 w-full opacity-100 hover:opacity-85 transition-all motion-reduce:transition-none">
-                        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-700 min-h-60">
-                            <img className="w-full" src="/covers/CPCalculator.png" alt="Cross Platform Calculator" />
-                            <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2 text-slate-200">Cross Platform Calculator</div>
-                                <p className="text-gray-400 text-base">
-                                    A calculator app with many features and themes. The app was made as a part of a team using Dart and Flutter.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="m-4">
-                <Link href="/EcoTrack">
-                    <div className="z-10 w-full opacity-100 hover:opacity-85 transition-all motion-reduce:transition-none">
-                        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-700 min-h-60">
-                            <img className="w-full" src="/covers/EcoTrack.png" alt="Angular To Do List" />
-                            <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2 text-slate-200">EcoTrack</div>
-                                <p className="text-gray-400 text-base">
-                                    EcoTrack is a web forum for discussing environmental issues and tracking progress on environmental initiatives.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-        </div>
+                    </Link>
+                </div>
+            ))}
+        </div >
     )
 }
