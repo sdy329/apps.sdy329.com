4d4c133604ba9a8ab6a878af3d3b9a020da726d3
sdy329 <sdy329@gmail.com>
2024-11-01 02:23:34 -0400

    Added disclaimer since deployment is not working as expected

--- src/app/EcoTrack/page.jsx
+++ src/app/EcoTrack/page.jsx
@@ -10,6 +10,10 @@ export default async function ResumePage() {
             <div className="mx-auto min-h-screen font-sans">
                 <ProjectNavBar link="https://github.com/sdy329/ecotrack" />
                 <div className="flex flex-col items-center justify-center my-4">
+                    <h1 className="text-4xl text-slate-200 font-bold text-center">Working on deployment</h1>
+                    <p className="text-lg text-center">Page may or may not load. Click the github icon to view the repository</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
                     <iframe src="https://ecotrack.42web.io/" className="w-full max-h-[90vh] h-[90vh]" />
                 </div>
             </div>
